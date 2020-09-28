acefe4a11d473220015b40a29df2432feb8591e7
aftilion <aftis.sansan@gmail.com>
2020-09-28 05:04:20 +0300

    -

--- lab1/six.sh
+++ lab1/six.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-awk '$3 == "(II)" {$3="Information:" ; print $0}' /var/log/anaconda/X.log > full.log
-
 awk '$3 == "(WW)" {$3="Warning:" ; print $0}' /var/log/anaconda/X.log > full.log
+
+awk '$3 == "(II)" {$3="Information:" ; print $0}' /var/log/anaconda/X.log >> full.log
 
